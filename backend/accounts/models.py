8a5bc9d9f79270d87341cc9e9aa3f4425d641810
betelhem demsis <bettydemsis@gmail.com>
2024-10-26 20:37:47 +0300

    add:

---
+++ backend/accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',  
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',  
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.username
---
+++ backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView,LoginView,LogoutView, MyProfile
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', MyProfile.as_view(), name='profile'),
+]
