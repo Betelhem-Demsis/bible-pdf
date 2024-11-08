ea6fca97d1a1ef865d5feffa4919f84d440cdeb9
betelhem demsis <bettydemsis@gmail.com>
2024-11-08 03:25:04 +0300

    modified

--- backend/bible/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.book_list, name='book_list'),
-#     path('load-data/', views.load_excel_data, name='load_excel_data'),
-#     path('<str:book_name>/<int:chapter_number>/', views.chapter_detail, name='chapter_detail'),
-# ]
